45760d342c2adf74d44171907176c6a1e4c0530f
kashinotti <ryo.ka.notti@gmail.com>
2021-02-23 22:55:38 +0900

    iOS  boot camp セクション6-26　終了

--- SwiftCountUp1/ViewController.swift
+++ SwiftCountUp1/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var countUpLabel: UILabel!
+    
+    
+    var count = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+//        ラベルの文字を0と表示する
+        countUpLabel.text = "0"
+        
+        
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        
+        
+//        カウントアップ
+        
+        
+        
+//        ラベルに文字を反映したい（数字）
+        
+        
+        
+//        カウントが10以上になったら黄色に変更する
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func minus(_ sender: Any) {
+      
+//        カウントダウン
+        
+        
+//        ラベルに文字を反映したい（数字）
+        
+        
+        
+        
+//        カウントが0以下になったら白に変更する
+        
+        
+        
+    }
+    
+    
 }
 
